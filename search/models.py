302b69afd3da13f6ae2d50d715c999f89d501205
Piellia Vasyl <pellia@ukr.net>
2015-09-02 12:53:21 +0300

    write process time into mylog.log

--- search/models.py
+++ search/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Search(models.Model):
     searching_text = models.CharField('Search text', max_length=150, default='put search text here')
     email = models.EmailField('E-mail to send response', default='put your email here')
-    timestamp = models.TimeField(auto_now=True, auto_now_add=False)
+    timestamp = models.DateTimeField(auto_now=True, auto_now_add=False)
 
     def __unicode__(self):
         return self.email
--- search/views.py
+++ search/views.py
@@ -1,14 +1,17 @@
+from datetime import time, datetime
 from django.core.mail import send_mail
 from django.conf import settings
-
+from django.utils import timezone
+from django.utils.timesince import timesince
 from django.shortcuts import render
-
+import logging
+from django.utils.timezone import utc
 
 from search.forms import SearchForm
 
 
 def home(request):
-    title = "Welcome"
+    title = "Search text"
     form = SearchForm(request.POST or None)
 
     context = {
@@ -17,26 +20,31 @@ def home(request):
     }
 
     if form.is_valid():
-        form.save()
+        instance = form.save(commit=False)
+        instance.save()
+        message = "You will get search results for: %s via %s soon" % (instance.searching_text, instance.email)
         context = {
             "title": "Thank you",
+            "message": message,
         }
 
         subject = 'Search results for: ' + form.cleaned_data.get('searching_text')
         message = "For %s: search results" % subject
         from_email = settings.EMAIL_HOST_USER
         to_email = form.cleaned_data.get('email')
+
+        logging.basicConfig(format=u'%(levelname)-8s [%(asctime)s] %(message)s', level=logging.DEBUG,
+                            filename=u'mylog.log')
+
+        time_diff = datetime.now(timezone.utc) - instance.timestamp
+
+        logging.info(time_diff.total_seconds())
+
         send_mail(subject,
                   message,
                   from_email,
                   [to_email],
                   fail_silently=False)
-
-        # mail.send(
-        #     'pellia@ukr.net',  # List of email addresses also accepted
-        #     'mybesttestmail@gmail.com',
-        #     subject='My email',
-        #     message='H1',
-        # )
-
     return render(request, "home.html", context)
+
+
